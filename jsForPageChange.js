f01278678689c3c1078c20f588bd901f07244307
sinsok <91297409+sinsok@users.noreply.github.com>
2021-09-24 11:55:27 +0900

    Update jsForPageChange.js

--- jsForPageChange.js
+++ jsForPageChange.js
@@ -2,7 +2,7 @@ var elm = document.getElementById('test_line');
 
 // パラメータの取得
 var param = location.search.substring(1);
-param = param.substring(param.indexOf('='));
+param = param.substring(param.indexOf('=') + 1);
 // パラメータの使用
 var elm = document.getElementById('test_line');    
 elm.textContent = param;
