7b08cd2d44606abd1304b4ecb9bb6b69f8878cc5
sinsok <91297409+sinsok@users.noreply.github.com>
2021-09-24 11:26:10 +0900

    Update jsForPageChange.js

--- jsForPageChange.js
+++ jsForPageChange.js
@@ -3,25 +3,14 @@ var elm = document.getElementById('test_line');
 // パラメータの取得
 var params = [];
 var param = location.search.substring(1);
-if(param !== '') {
-    param = param.split('&');
-}
-for(var i = 0; i < param.length; i++) {
-    params[i] = param[i].substring(param[i].indexOf('='));
-}
- 
+param = param.substring(param.indexOf('='));
 
 // パラメータの使用
-var target = 'url'
-for(var i = 0; i < params.length; i++) {
-    if(params[i][0] == target) {
-        var elm = document.getElementById('test_line');    
-        elm.textContent = params[i][1];
+var elm = document.getElementById('test_line');    
+elm.textContent = param;
       
-        var repUrl = params[i][1];
-        repUrl = repUrl.replace("watch?v=", "embed/");
-        repUrl = repUrl.replace("&feature=share", "");
-        var iframeElm = document.getElementById('InjectionIframe');    
-        iframeElm.src = repUrl;
-    }
-}
+var repUrl = param;
+repUrl = repUrl.replace("watch?v=", "embed/");
+repUrl = repUrl.replace("&feature=share", "");
+var iframeElm = document.getElementById('InjectionIframe');    
+iframeElm.src = repUrl;
