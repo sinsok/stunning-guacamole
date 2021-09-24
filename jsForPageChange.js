09d758f43ac82063a4bddc14c304a5b119684dd4
sinsok <91297409+sinsok@users.noreply.github.com>
2021-09-24 11:46:20 +0900

    Update jsForPageChange.js

--- jsForPageChange.js
+++ jsForPageChange.js
@@ -2,21 +2,13 @@ var elm = document.getElementById('test_line');
 
 // パラメータの取得
 var param = location.search.substring(1);
-if(param.indexOf("url") > 0)
-{
-      param = param.substring(param.indexOf('='));
-      // パラメータの使用
-      var elm = document.getElementById('test_line');    
-      elm.textContent = param;
+param = param.substring(param.indexOf('='));
+// パラメータの使用
+var elm = document.getElementById('test_line');    
+elm.textContent = param;
 
-      var repUrl = param;
-      repUrl = repUrl.replace("watch?v=", "embed/");
-      repUrl = repUrl.replace("&feature=share", "");
-      var iframeElm = document.getElementById('InjectionIframe');    
-      iframeElm.src = repUrl;
-}
-else
-{
-      var elm = document.getElementById('test_line');    
-      elm.textContent = "failed!";
-}
+var repUrl = param;
+repUrl = repUrl.replace("watch?v=", "embed/");
+repUrl = repUrl.replace("&feature=share", "");
+var iframeElm = document.getElementById('InjectionIframe');    
+iframeElm.src = repUrl;
