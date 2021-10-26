8c10c71fdaf620d33a4f5ecbcabedf7b19fbb618
sinsok <91297409+sinsok@users.noreply.github.com>
2021-10-26 10:19:08 +0900

    Update jsForPageChange.js

--- jsForPageChange.js
+++ jsForPageChange.js
@@ -5,10 +5,26 @@ var param = location.search.substring(1);
 param = param.substring(param.indexOf('=') + 1);
 // パラメータの使用
 var elm = document.getElementById('test_line');    
-elm.textContent = param;
-
 var repUrl = param;
-repUrl = repUrl.replace("watch?v=", "embed/");
-repUrl = repUrl.replace("&feature=share", "");
+
+//プレイリストのキーワード「list=」で区切る
+var splitAarray = param.split('list=');
+
+//キーワードが一つでも含まれている場合
+if(splitAarray.length > 0)
+{
+	//プレイリストの処理
+	repUrl = 'https://www.youtube.com/embed/videoseries?list=' + splitAarray[1];
+  elm.textContent = param + '(プレイリスト)';
+}
+else
+{
+	//動画の処理
+	repUrl = repUrl.replace("watch?v=", "embed/");
+	repUrl = repUrl.replace("&feature=share", "");
+  elm.textContent = param + '(動画)';
+}
+
 var iframeElm = document.getElementById('InjectionIframe');    
 iframeElm.src = repUrl;
+
